14ebae206f89628e83020933c263d200fba0ba7d
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-02-18 23:02:55 +0000

    Correcly displays the side-by-side images

---
+++ assets/js/images.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', function () {
+  var groups = document.querySelectorAll('.image-group');
+  Array.prototype.slice.call(groups).forEach(function (group) {
+    var images = Array.prototype.slice.call(group.children)
+      .map(function (div) {
+        var image = div.children[0];
+        var w = image.width;
+        var h = image.height;
+        var r = w / h;
+        return { width: w, height: h, ratio: r };
+      });
+
+    var maxWidth = 780 - 5; // 5px margin
+
+    var percentage = (images[0].ratio / ( images[0].ratio +  images[1].ratio)) * (maxWidth / 780);
+    group.children[0].style.flexBasis = (percentage * 100) + '%';
+    group.children[0].style.flexShrink = '0';
+  });
+});
